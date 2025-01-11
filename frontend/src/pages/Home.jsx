75595e4207d5dcc02faeb2712775de8807939e3f
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2025-01-11 21:39:44 +0530

    Uber clone app pick-up and destination form animation done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
 
 const Home = () => {
+
+  const [pickup, setPickup] = useState('');
+  const [destination, setDestination] = useState('');
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%',
+        padding: 24
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 1
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0%',
+        padding: 0
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 0
+      });
+    }
+  }, [panelOpen]);
+
   return (
-    <div>Home</div>
-  )
+    <div className="relative h-screen overflow-hidden">
+      <img className="absolute w-16 left-5 top-5" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+
+      <div className="w-screen h-screen">
+        <img className="object-cover w-full h-full" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+      </div>
+
+      <div className="absolute top-0 flex flex-col justify-end w-full h-screen p-5 ">
+
+        <div className="h-[30%] p-6 bg-white relative">
+
+          <h5 ref={panelCloseRef} onClick={() => {
+            setPanelOpen(false)
+          }} className='absolute text-2xl opacity-0 right-6 top-6'>
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+
+          <form onSubmit={submitHandler} className="relative py-3">
+            <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full"
+              type="text"
+              placeholder="Add a pick-up location"
+            />
+            <input
+              onClick={() => {
+                setPanelOpen(true);
+              }}
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+              type="text"
+              placeholder="Enter your destination"
+            />
+          </form>
+
+          {/* <button
+            className='w-full px-4 py-2 mt-3 text-white bg-black rounded-lg'>
+            Find Trip
+          </button> */}
+        </div>
+
+        <div ref={panelRef} className="h-[0%] bg-white  p-5 overflow-hidden transition-all duration-300"></div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
